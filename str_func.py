37242355758fa6d2c937814b017773228f883c8c
Anton Pashinov PC <pashinov1988@gmail.com>
2024-03-20 16:34:16 +0700

    Add title_func() to str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def upper_func(string: str) -> str:
+    """Это док-стринга. Корявая такая"""
     return string.upper()
+
+
+def title_func(string: str) -> str:
+    """Функция возвращает строку, в которой каждое слово начинается с заглавной буквы"""
+    return string.title()
