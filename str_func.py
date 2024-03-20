5d1b860822709dfb359cc87490335195c0319ec5
Anton Pashinov PC <pashinov1988@gmail.com>
2024-03-20 17:46:14 +0700

    Исправлен баг. Хоть он и не баг вовсе

--- str_func.py
+++ str_func.py
@@ -1,8 +1,10 @@
 def upper_func(string: str) -> str:
     """Функция возвращает строку в виде заглавных букв."""
-    return string.upper()
+    text = string.upper()
+    return text
 
 
 def title_func(string: str) -> str:
     """Функция возвращает строку, где каждое слово с заглавной буквы."""
-    return string.title()
+    text = string.title()
+    return text
